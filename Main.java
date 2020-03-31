7b349b0afcde8cf84bdecc7bbf30ce806fc00181
HanseLopez99 <halm29799@gmail.com>
2020-03-30 22:59:58 -0600

    First Commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+/*Hansel Andre Lopez Montenegro 19026
+Algoritmos y Estructura de datos
+Hoja de trabajo 8
+*/
+
+public class Main{
+    public static void main(String arg[]){
+        
+    }
+}
---
+++ fileRead.java
@@ -0,0 +1,40 @@
+public class fileRead{
+    ArrayList<String> list;
+    String[] parts;
+
+	public ArrayList<String> Reader(){
+        list = new ArrayList<String>();
+
+		String fileName = "cards_desc.txt";
+
+        String line = null;
+
+        try {
+
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                parts = line.split("\\, ");
+                list.add(parts[0]);
+                parts = null;
+            }   
+
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+        }
+        return list;
+    }
+}
